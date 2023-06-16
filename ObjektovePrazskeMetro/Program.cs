7594b7b6f8809778c7afec7dd4e65e5ee8c76182
Michal Helgert <michal.helgert@seznam.cz>
2023-06-16 16:35:00 +0200

    uwuw

--- ObjektovePrazskeMetro/Program.cs
+++ ObjektovePrazskeMetro/Program.cs
@@ -1,63 +1,29 @@
 ﻿using ObjektovePrazskeMetro;
 using Spectre.Console;
 
-//string[] poleLinkaA = { "Nemocnice Motol", "Petřiny", "Nádraží Veleslavní", "Bořislavka", "Dejvická", "Hradčanská", "Malostranská", "Staroměstská",
-//          "Můstek", "Muzeum", "Náměstí míru", "Jiřího z Poděbrad", "Flora", "Želivského", "Strašnická", "Skalka", "Depo Hostivař" };
-//string[] poleLinkaB = { "Zličín", "Stodůlky", "Luka", "Lužiny", "Hůrka", "Nové Butovice", "Jinonice", "Radlická", "Smíchovské nádraží", "Anděl", "Karlovo náměstí",
-//          "Národní třída", "Můstek", "Náměstí Republiky", "Florenc", "Křížova", "Invalidovna", "Českomoravská", "Vysočasnská", "Kolbenova", "Hloubětín", "Rajská zahrada", "Černý Most"};
-//string[] poleLinkaC = {"Letňany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní nádraží", "Muzeum",
-//          "I. P. Pavlova", "Vyšehrad", "Pražského povstání", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "Chodov", "Opatov", "Háje"};
-//string[] poleLinkaD = { "Depo Písnice", "Písnice", "Libuš", "Nové Dvory", "Nemocnice Krč", "Nádraží Krč", "Olbrachtova", "Pankrác", "Náměstí Bratří Synků", "Náměstí Míru" };
+string[] poleLinkaA = { "Nemocnice Motol", "Petřiny", "Nádraží Veleslavní", "Bořislavka", "Dejvická", "Hradčanská", "Malostranská", "Staroměstská",
+          "Můstek", "Muzeum", "Náměstí míru", "Jiřího z Poděbrad", "Flora", "Želivského", "Strašnická", "Skalka", "Depo Hostivař" };
+string[] poleLinkaB = { "Zličín", "Stodůlky", "Luka", "Lužiny", "Hůrka", "Nové Butovice", "Jinonice", "Radlická", "Smíchovské nádraží", "Anděl", "Karlovo náměstí",
+          "Národní třída", "Můstek", "Náměstí Republiky", "Florenc", "Křížova", "Invalidovna", "Českomoravská", "Vysočasnská", "Kolbenova", "Hloubětín", "Rajská zahrada", "Černý Most"};
+string[] poleLinkaC = {"Letňany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní nádraží", "Muzeum",
+          "I. P. Pavlova", "Vyšehrad", "Pražského povstání", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "Chodov", "Opatov", "Háje"};
+string[] poleLinkaD = { "Depo Písnice", "Písnice", "Libuš", "Nové Dvory", "Nemocnice Krč", "Nádraží Krč", "Olbrachtova", "Pankrác", "Náměstí Bratří Synků", "Náměstí Míru" };
 
-//Console.WriteLine("Zadej linku kterou chceš využít: (A/B/C/D)");
-//string volbaLinky = Console.ReadLine();
-//if(volbaLinky == "A")
-//{
-//    LinkaMetra linkaA = new LinkaMetra() 
-//    { NazevLinky = "A", Barva = "zelená", Aktivni = true, Stanice = poleLinkaA };
-//    linkaA.NastavLinku();
-//    Console.WriteLine("Chcete vypsat 1. všechny stanice | 2. první a poslední | 3. zjistit čas cesty");
-//    string volba = Console.ReadLine();
-//    if(volba == "1")
-//    {
-//        linkaA.VypisVsechnyStanice();
-//    }
-//    else if(volba == "2")
-//    {
-//        linkaA.VypisPrvniPosledni();
-//    }
-//}
-
-
-
-
-
-int max = 0;
-while (true)
+Console.WriteLine("Zadej linku kterou chceš využít: (A/B/C/D)");
+string volbaLinky = Console.ReadLine();
+if (volbaLinky == "A")
 {
-    Console.WriteLine("Zadej hodnotu na šířku monitoru");
-    int c1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("zadej hodnotu na délku monitoru");
-    int c2 = Convert.ToInt32(Console.ReadLine());
-    if (c1 <= 4000 || c2 <= 3000)
+    LinkaMetra linkaA = new LinkaMetra()
+    { NazevLinky = "A", Barva = "zelená", Aktivni = true, Stanice = poleLinkaA };
+    linkaA.NastavLinku();
+    Console.WriteLine("Chcete vypsat 1. všechny stanice | 2. první a poslední | 3. zjistit čas cesty");
+    string volba = Console.ReadLine();
+    if (volba == "1")
     {
-        Console.WriteLine(c1 * c2);
-        int min = c1 * c2;
-        if (max < min)
-        {
-            max = min;
-            Console.WriteLine(max);
-        }
-        if (min > 4000000)
-        {
-            Console.WriteLine("Hodně");
-            break;
-        }
+        linkaA.VypisVsechnyStanice();
     }
-    else if (c1 < 0 && c2 < 0)
+    else if (volba == "2")
     {
-        Console.WriteLine("malo");
-        break;
+        linkaA.VypisPrvniPosledni();
     }
 }
-Console.WriteLine(max);
