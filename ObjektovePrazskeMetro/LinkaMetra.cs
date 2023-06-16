5dedd765f17a15ee296a6bfa8adad6655b34480b
Michal Helgert <michal.helgert@seznam.cz>
2023-06-16 12:34:29 +0200

    Přidejte soubory projektu.

---
+++ ObjektovePrazskeMetro/LinkaMetra.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektovePrazskeMetro
+{
+    public class LinkaMetra
+    {
+        public string NazevLinky { get; set; }
+        public string[] Stanice { get; set; }
+        public bool Aktivni { get; set; }
+        public string Barva { get; set; }
+
+        public void VypisVsechnyStanice()
+        {
+            if (Stanice.Length > 0) 
+            {
+                foreach (var jednaStanice in Stanice)
+                {
+                    Console.WriteLine(jednaStanice);
+                }
+            }
+        }
+        public void VypisPrvniPosledni()
+        {
+            if(Stanice.Length >= 2)
+            {
+                Console.WriteLine(Stanice[0]); //první
+                Console.WriteLine(Stanice[Stanice.Length-1]); //poslední
+            }
+        }
+        public void NastavLinku()
+        {
+            if(Barva == "zelená")
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+            }
+            else if(Barva == "červená")
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+            }
+            else if(Barva == "žlutá")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if(Barva == "modrá")
+            {
+                Console.ForegroundColor= ConsoleColor.Blue;
+            }
+            Console.WriteLine($"Vítej v lince metra {NazevLinky}");
+        }
+        public void CasCesty()
+        {
+            VypisVsechnyStanice();
+            Console.WriteLine("***************************");
+            Console.WriteLine("Vyber 1. stanici");
+            string prvniStanice = Console.ReadLine();
+            int indexPrvni = Array.IndexOf(Stanice, prvniStanice); //např. 1
+            Console.WriteLine("---------");
+
+            Console.WriteLine("Vyber 2. stanici");
+            string druhaStanice = Console.ReadLine();
+            int indexDruhe = Array.IndexOf(Stanice, druhaStanice); //např. 4
+            Console.WriteLine("---------");
+            int dobaCesty = (indexPrvni - indexDruhe)*2;
+            Console.WriteLine("Doba cesty: " + dobaCesty);
+        }
+    }
+}
---
+++ ObjektovePrazskeMetro/Program.cs
@@ -0,0 +1,28 @@
+﻿using ObjektovePrazskeMetro;
+
+string[] poleLinkaA = { "Nemocnice Motol", "Petřiny", "Nádraží Veleslavní", "Bořislavka", "Dejvická", "Hradčanská", "Malostranská", "Staroměstská",
+          "Můstek", "Muzeum", "Náměstí míru", "Jiřího z Poděbrad", "Flora", "Želivského", "Strašnická", "Skalka", "Depo Hostivař" };
+string[] poleLinkaB = { "Zličín", "Stodůlky", "Luka", "Lužiny", "Hůrka", "Nové Butovice", "Jinonice", "Radlická", "Smíchovské nádraží", "Anděl", "Karlovo náměstí",
+          "Národní třída", "Můstek", "Náměstí Republiky", "Florenc", "Křížova", "Invalidovna", "Českomoravská", "Vysočasnská", "Kolbenova", "Hloubětín", "Rajská zahrada", "Černý Most"};
+string[] poleLinkaC = {"Letňany", "Prosek", "Střížkov", "Ládví", "Kobylisy", "Nádraží Holešovice", "Vltavská", "Florenc", "Hlavní nádraží", "Muzeum",
+          "I. P. Pavlova", "Vyšehrad", "Pražského povstání", "Pankrác", "Budějovická", "Kačerov", "Roztyly", "Chodov", "Opatov", "Háje"};
+string[] poleLinkaD = { "Depo Písnice", "Písnice", "Libuš", "Nové Dvory", "Nemocnice Krč", "Nádraží Krč", "Olbrachtova", "Pankrác", "Náměstí Bratří Synků", "Náměstí Míru" };
+
+Console.WriteLine("Zadej linku kterou chceš využít: (A/B/C/D)");
+string volbaLinky = Console.ReadLine();
+if(volbaLinky == "A")
+{
+    LinkaMetra linkaA = new LinkaMetra() 
+    { NazevLinky = "A", Barva = "zelená", Aktivni = true, Stanice = poleLinkaA };
+    linkaA.NastavLinku();
+    Console.WriteLine("Chcete vypsat 1. všechny stanice | 2. první a poslední | 3. zjistit čas cesty");
+    string volba = Console.ReadLine();
+    if(volba == "1")
+    {
+        linkaA.VypisVsechnyStanice();
+    }
+    else if(volba == "2")
+    {
+        linkaA.VypisPrvniPosledni();
+    }
+}
